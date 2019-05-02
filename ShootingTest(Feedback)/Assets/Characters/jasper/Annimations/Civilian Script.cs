66f8799c7fd34d8cb34f815a54f6b56a1dc849bd
IIMakII <37370335+IIMakII@users.noreply.github.com>
2019-05-02 23:06:04 +0100

    testing characters

---
+++ ShootingTest(Feedback)/Assets/Characters/jasper/Annimations/Civilian Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivilianScript : MonoBehaviour
+{
+    GameObject player;
+    float distFromPlayer;
+    [SerializeField] float PlayerDistToScare;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distFromPlayer = Vector3.Distance(player.transform.position, this.transform.position);
+
+       
+    }
+}
---
+++ ShootingTest(Feedback)/Assets/Scripts/Civilian Script1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivilianScript1 : MonoBehaviour
+{
+    GameObject player;
+    float distFromPlayer;
+    [SerializeField] float PlayerDistToScare;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distFromPlayer = Vector3.Distance(player.transform.position, this.transform.position);
+
+
+    }
+}
