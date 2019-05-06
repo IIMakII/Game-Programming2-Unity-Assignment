95d312ff07c471cfb4cc248336794237ce8f69e5
IIMakII <37370335+IIMakII@users.noreply.github.com>
2019-05-06 15:42:49 +0100

    main menu attempt

--- ShootingTest(Feedback)/Assets/Scripts/GameManagerScript.cs
+++ ShootingTest(Feedback)/Assets/Scripts/GameManagerScript.cs
@@ -2,24 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using UnityEngine.AI;
 using System;
 using System.IO;
 
 public class GameManagerScript : MonoBehaviour
 {
-      [SerializeField] GameObject player;
+      [SerializeField]  GameObject player;
     [SerializeField]  GameObject [] Civilians;
     [SerializeField] GameObject[] NPCs;
+    public static bool ToResume = false;
 
 
-    // Start is called before the first frame update
+
     private void start()
     {
        
         Civilians = GameObject.FindGameObjectsWithTag("Civilian");
         NPCs = GameObject.FindGameObjectsWithTag("NPC");
-        GetState();
+        if(ToResume == true)
+        {
+            GetState();
+            ToResume = false;
+        }
+       
+    }
+
+    public void ResumeGame()
+    {
+        ToResume = true;
+        SceneManager.LoadScene(1);
+    }
+
+    public  void NewGame()
+    {
+        SceneManager.LoadScene(1);
     }
 
     // Update is called once per frame
@@ -48,20 +66,26 @@ public class GameManagerScript : MonoBehaviour
 
     private void SetState()
     {
-        PlayerPrefs.SetFloat("Xpos", player.transform.position.x);
-        PlayerPrefs.SetFloat("Ypos", player.transform.position.y);
-        PlayerPrefs.SetFloat("Zpos", player.transform.position.z);
-        PlayerPrefs.SetInt("MainGunAmmo", player.GetComponentInParent<MainGun>().currentAmmo);
-        PlayerPrefs.SetInt("BoopGunAmmo", player.GetComponentInParent<BoopGun>().currentAmmo);
-        PlayerPrefs.SetFloat("Score", player.GetComponentInParent<PlayerScript>().points);
+        if(player != null)
+        {
+            PlayerPrefs.SetFloat("Xpos", player.transform.position.x);
+            PlayerPrefs.SetFloat("Ypos", player.transform.position.y);
+            PlayerPrefs.SetFloat("Zpos", player.transform.position.z);
+            PlayerPrefs.SetInt("MainGunAmmo", player.GetComponentInParent<MainGun>().currentAmmo);
+            PlayerPrefs.SetInt("BoopGunAmmo", player.GetComponentInParent<BoopGun>().currentAmmo);
+            PlayerPrefs.SetFloat("Score", player.GetComponentInParent<PlayerScript>().points);
+        }
     }
 
     private void GetState()
     {
-        player.transform.position = new Vector3(PlayerPrefs.GetFloat("Xpos"), PlayerPrefs.GetFloat("Ypos"), PlayerPrefs.GetFloat("Zpos"));
-        player.GetComponentInParent<MainGun>().currentAmmo = PlayerPrefs.GetInt("MainGunAmmo");
-        player.GetComponentInParent<BoopGun>().currentAmmo = PlayerPrefs.GetInt("BoopGunAmmo");
-        player.GetComponentInParent<PlayerScript>().points = PlayerPrefs.GetFloat("Score");
+        if(player != null)
+        {
+            player.transform.position = new Vector3(PlayerPrefs.GetFloat("Xpos"), PlayerPrefs.GetFloat("Ypos"), PlayerPrefs.GetFloat("Zpos"));
+            player.GetComponentInParent<MainGun>().currentAmmo = PlayerPrefs.GetInt("MainGunAmmo");
+            player.GetComponentInParent<BoopGun>().currentAmmo = PlayerPrefs.GetInt("BoopGunAmmo");
+            player.GetComponentInParent<PlayerScript>().points = PlayerPrefs.GetFloat("Score");
+        }
     }
 
 }
